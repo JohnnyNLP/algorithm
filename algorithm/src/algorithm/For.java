ec9694c25f51fd1a81b8bda13810e1f64e7202f0
uakbuak <pchmarine@naver.com>
2019-04-15 23:52:02 +0900

    #2739 구구단 문제

--- algorithm/src/algorithm/For.java
+++ algorithm/src/algorithm/For.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class For {
 	
-	
+	// 2739 구구단 2단 출력하기
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		for (int i=1; i<=9; i++) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}
+	}
 	
 	// 2742 5부터 1까지 출력하기
 //	public static void main(String[] args) {
