6eb72104860adcd2dfac05518a337808849aa22d
uakbuak <pchmarine@naver.com>
2019-04-15 19:52:29 +0900

    hello world

---
+++ algorithm/src/algorithm/Main.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	// 2557 Hello World!
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
