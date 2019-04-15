da6de0854b21617069958161d3e1f025088bfd90
uakbuak <pchmarine@naver.com>
2019-04-15 19:53:05 +0900

    A+B

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 	
-	// 2557 Hello World!
+	// 1000 A+B ¿‘√‚∑¬
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		 Scanner scan = new Scanner(System.in);
+		 int A = scan.nextInt();
+		 int B = scan.nextInt();
+		 System.out.println(A+B);
 	}
+
+	
+	// 2557 Hello World!
+//	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//	}
 
 }
