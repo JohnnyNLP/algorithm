733fa19536954e5e91512907fd8ea22bc5f8fba3
uakbuak <pchmarine@naver.com>
2019-04-16 00:02:21 +0900

    #2438 별찍기 문제

--- algorithm/src/algorithm/For.java
+++ algorithm/src/algorithm/For.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 
 public class For {
 	
-	// 2739 구구단 2단 출력하기
+	// 2438 별 찍기
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
-		for (int i=1; i<=9; i++) {
-			System.out.println(a+" * "+i+" = "+a*i);
+		for (int i=1; i<=a; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
+	
+	// 2739 구구단 2단 출력하기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int a = scan.nextInt();
+//		for (int i=1; i<=9; i++) {
+//			System.out.println(a+" * "+i+" = "+a*i);
+//		}
+//	}
 	
 	// 2742 5부터 1까지 출력하기
 //	public static void main(String[] args) {
