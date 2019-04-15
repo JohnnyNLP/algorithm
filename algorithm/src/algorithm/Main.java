369b6e4f36d0c19ea9816f6d844a7a3416c41bf2
uakbuak <pchmarine@naver.com>
2019-04-15 19:59:25 +0900

    #7287

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 	
-	// 1000 A+B 입출력
+	// 7287 맞춘 문제, ID 출력
 	public static void main(String[] args) {
-		 Scanner scan = new Scanner(System.in);
-		 int A = scan.nextInt();
-		 int B = scan.nextInt();
-		 System.out.println(A+B);
+		System.out.println(3);
+		System.out.println("pch7678");
 	}
+	
+	// 1000 A+B 입출력
+//	public static void main(String[] args) {
+//		 Scanner scan = new Scanner(System.in);
+//		 int A = scan.nextInt();
+//		 int B = scan.nextInt();
+//		 System.out.println(A+B);
+//	}
 
 	
 	// 2557 Hello World!
