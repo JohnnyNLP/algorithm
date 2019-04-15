0158c9208d62b9443c87e900c6a4c4a1a691fa8c
uakbuak <pchmarine@naver.com>
2019-04-15 23:48:17 +0900

    #2741, 2742 for문

---
+++ algorithm/src/algorithm/For.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class For {
+	
+	
+	
+	// 2742 5부터 1까지 출력하기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int a = scan.nextInt();
+//		for (int i=a; i>=1; i--) {
+//			System.out.println(i);
+//		}
+//	}
+	
+	
+	// 2741 1부터 5까지 출력하기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int a = scan.nextInt();
+//		for (int i=1; i<=a; i++) {
+//			System.out.println(i);
+//		}
+//	}
+
+}
