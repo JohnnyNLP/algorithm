7530ba6564db83d5d584ca4baf1e4aad8de434e5
uakbuak <pchmarine@naver.com>
2019-04-15 21:18:57 +0900

    #2839 sugar delivery

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -4,19 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 	
-	// 10430 나머지, 몫 계산하기
+	// 2839 설탕 배달 알고리즘
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int A = scan.nextInt();
-		int B = scan.nextInt();
-		int C = scan.nextInt();
-		System.out.println((A+B)%C);
-		System.out.println((A%C + B%C)%C);
-		System.out.println((A*B)%C);
-		System.out.println((A%C*B%C)%C);
-		}
+		int sugar = scan.nextInt();
+		int n = -1;
+		int a = sugar/5;
+		int b = sugar/3;
 		
+		for (int i = 0; i <=a; i++) {
+			for (int j = 0; j <=b; j++) {
+				int p = 5*i;
+				int k = 3*j;
+				if (p+k == sugar) {
+					n = p/5 + k/3;
+				}
+			}
+		}
+		System.out.println(n);
 	}
+
+	
+	// 10430 나머지, 몫 계산하기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in); 
+//		int A = scan.nextInt();
+//		int B = scan.nextInt();
+//		int C = scan.nextInt();
+//		System.out.println((A+B)%C);
+//		System.out.println((A%C + B%C)%C);
+//		System.out.println((A*B)%C);
+//		System.out.println((A%C*B%C)%C);
+//		}
+//	}
 	
 	// 11717 여러 줄 출력하기
 //	public static void main(String[] args) {
