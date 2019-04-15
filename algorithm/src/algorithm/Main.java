5a0a3480e1529a288953240b39eaa32d25521eb0
uakbuak <pchmarine@naver.com>
2019-04-15 20:18:20 +0900

    11717 multiple line print

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -4,14 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 	
-	// 10172 개 출력하기
+	// 11717 여러 줄 출력하기
 	public static void main(String[] args) {
-		System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            String input = scan.nextLine();
+            System.out.println(input);
+        }
 	}
+	
+	// 10172 개 출력하기
+//	public static void main(String[] args) {
+//		System.out.println("|\\_/|");
+//		System.out.println("|q p|   /}");
+//		System.out.println("( 0 )\"\"\"\\");
+//		System.out.println("|\"^\"`    |");
+//		System.out.println("||_/=\\\\__|");
+//	}
 	
 	// 7287 맞춘 문제, ID 출력
 //	public static void main(String[] args) {
