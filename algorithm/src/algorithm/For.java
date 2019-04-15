3f350771983a02e32127262e860d49645d41dd2a
uakbuak <pchmarine@naver.com>
2019-04-16 00:21:44 +0900

    #2441 별 뒤집어 반대로 찍기

--- algorithm/src/algorithm/For.java
+++ algorithm/src/algorithm/For.java
@@ -3,18 +3,33 @@ package algorithm;
 import java.util.Scanner;
 
 public class For {
-		
-	// 2440 별 반대로 찍기
+	
+	// 2441 별 뒤집어 반대로 찍기
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		for (int i=a; i>=1; i--) {
-			for(int j=1; j<=i; j++) {
+			for(int j=1; j<=a-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
+	
+	// 2440 별 반대로 찍기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int a = scan.nextInt();
+//		for (int i=a; i>=1; i--) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
 	
 	// 2439 별 거꾸로 찍기
 //	public static void main(String[] args) {
