179a50f9f64558dbd0b57c6e02082092f035239c
uakbuak <pchmarine@naver.com>
2019-04-17 18:59:15 +0900

    #1547 점수 조작하기

--- algorithm/src/algorithm/If.java
+++ algorithm/src/algorithm/If.java
@@ -1,26 +1,73 @@
 package algorithm;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class If {
 	
-	//10871 X보다 작은 수 출력하기
+	//1547 점수 조작하기 2
+	//Scanner 객체 사용 시 응답 시간 : 160 ms
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		int X = scan.nextInt();
-		ArrayList <Integer> list = new ArrayList();
-		for (int i=1; i<=N; i++) {
-			int temp = scan.nextInt();
-			if (temp < X) {
-				list.add(temp);
-			}
-		}
-		for (int i : list) {
-			System.out.print(i+" ");
-		}
+        int count = scan.nextInt();
+        int [] grades = new int[count];
+        for(int i=0; i<count; i++) {
+            grades[i] = scan.nextInt();
+        }
+        Arrays.sort(grades);
+        double M = grades[grades.length-1];
+        double sum = 0;
+        for(int i=0; i<count; i++) {
+            sum += grades[i]/M*100;
+        }
+        System.out.println(sum/count);
 	}
+	
+	//1547 점수 조작하기 1
+	//Buffered 객체 사용 시 응답 시간 : 84 ms
+//	public static void main(String[] args) {
+//		try {
+//			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//			int count = Integer.parseInt(bf.readLine());
+//			StringTokenizer st = new StringTokenizer(bf.readLine());
+//			int[] grades = new int[count];
+//			
+//			for(int i=0; i<count; i++) {
+//				grades[i] = Integer.parseInt(st.nextToken());
+//			}
+//			Arrays.sort(grades);
+//			double M = grades[grades.length-1];
+//			double sum = 0;
+//			for(int i=0; i<count; i++) {
+//				sum += grades[i]/M*100;
+//			}
+//			bw.write(Double.toString(sum/count));
+//			bw.flush();
+//			bw.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	
+	//10871 X보다 작은 수 출력하기
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int N = scan.nextInt();
+//		int X = scan.nextInt();
+//		ArrayList <Integer> list = new ArrayList();
+//		for (int i=1; i<=N; i++) {
+//			int temp = scan.nextInt();
+//			if (temp < X) {
+//				list.add(temp);
+//			}
+//		}
+//		for (int i : list) {
+//			System.out.print(i+" ");
+//		}
+//	}
 	
 	//10817 세 숫자 비교
 //	public static void main(String[] args) {
