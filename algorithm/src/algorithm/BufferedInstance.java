bcca3570dacfb9cc89abd1a3ae6619e9fedf39af
uakbuak <pchmarine@naver.com>
2019-04-18 02:20:32 +0900

    Buffered 객체 미리 만들어두기

---
+++ algorithm/src/algorithm/BufferedInstance.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BufferedInstance {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			String blah = br.readLine();
+			
+			// ¿€º∫∫Œ
+			
+			bw.write(blah);
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
