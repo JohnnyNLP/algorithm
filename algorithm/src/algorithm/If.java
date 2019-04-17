8c0536695d18e501aa2456ec227e023119ec1fcc
uakbuak <pchmarine@naver.com>
2019-04-17 17:59:42 +0900

    #10871 X보다 작은값 출력

--- algorithm/src/algorithm/If.java
+++ algorithm/src/algorithm/If.java
@@ -1,31 +1,49 @@
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class If {
 	
-	//10817 세 숫자 비교
+	//10871 X보다 작은 수 출력하기
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int A = scan.nextInt();
-		int B = scan.nextInt();
-		int C = scan.nextInt();
-		if (A>B && A>C) {
-			if (B>=C) {
-				System.out.println(B);
-			} else {
-				System.out.println(C);
+		int N = scan.nextInt();
+		int X = scan.nextInt();
+		ArrayList <Integer> list = new ArrayList();
+		for (int i=1; i<=N; i++) {
+			int temp = scan.nextInt();
+			if (temp < X) {
+				list.add(temp);
 			}
-		} else if (A<B && A<C) {
-			if (B>=C) {
-				System.out.println(C);
-			} else {
-				System.out.println(B);
-			}
-		} else {
-			System.out.println(A);
+		}
+		for (int i : list) {
+			System.out.print(i+" ");
 		}
 	}
+	
+	//10817 세 숫자 비교
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int A = scan.nextInt();
+//		int B = scan.nextInt();
+//		int C = scan.nextInt();
+//		if (A>B && A>C) {
+//			if (B>=C) {
+//				System.out.println(B);
+//			} else {
+//				System.out.println(C);
+//			}
+//		} else if (A<B && A<C) {
+//			if (B>=C) {
+//				System.out.println(C);
+//			} else {
+//				System.out.println(B);
+//			}
+//		} else {
+//			System.out.println(A);
+//		}
+//	}
 	
 	
 //	// 9498 시험 성적 매기기
