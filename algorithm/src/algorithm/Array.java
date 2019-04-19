1235134adbd830d822be6f19880ae42545f4d6ca
uakbuak <pchmarine@naver.com>
2019-04-19 23:17:59 +0900

    #2577 숫자 세기

--- algorithm/src/algorithm/Array.java
+++ algorithm/src/algorithm/Array.java
@@ -2,29 +2,49 @@ package algorithm;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Array {
 
-	public static void main(String[] args) {
-		//1152 단어 세기
+	//2577 숫자 세기
+	public static void main(String[]args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int gob = A*B*C;
+		int length = (int)(Math.log10(gob)+1);
 		
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			String sentence = br.readLine();
-			StringTokenizer st = new StringTokenizer(sentence);
-			int count = st.countTokens();
-			bw.write(Integer.toString(count));
-			bw.flush();
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		int[] array = new int[10];
+		
+		for(int i = 1; i <= length; i ++) {
+			array[gob%10] += 1;
+			gob /= 10;
 		}
+		
+		for (int i : array) {
+			bw.write(Integer.toString(i)+"\n");
+		}
+		bw.flush();
+		bw.close();
 	}
+	
+	//1152 단어 세기
+//	public static void main(String[] args) {
+//		try {
+//			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//			String sentence = br.readLine();
+//			StringTokenizer st = new StringTokenizer(sentence);
+//			int count = st.countTokens();
+//			bw.write(Integer.toString(count));
+//			bw.flush();
+//			bw.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 }
