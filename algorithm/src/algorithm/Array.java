0e552ccec00cf286b339fd0e5934948afc59188e
uakbuak <pchmarine@naver.com>
2019-04-19 23:56:47 +0900

    2920 음계 문제

--- algorithm/src/algorithm/Array.java
+++ algorithm/src/algorithm/Array.java
@@ -1,38 +1,61 @@
 package algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Array {
 	
-	//8958 OX 퀴즈
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int count = Integer.parseInt(br.readLine());
-		String line;
-		
-		for (int i = 1; i <= count; i++) {
-			int score = 0;
-			int[] array = new int[2];
-			line = br.readLine();
-			char[] chars = line.toCharArray();
-			for (char c : chars) {
-				if(c == 'O') {
-					array[0] += 1;
-					score += array[0];
-				} else if(c == 'X') {
-					array[1] += 1;
-					array[0] = 0;
-				}
-			}
-			bw.write(Integer.toString(score)+"\n");
+	//2920 음계
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[8];
+		for (int i = 0; i <= 7; i ++) {
+			array[i] = scan.nextInt();
 		}
-		bw.flush();
-		bw.close();
+		int temp = 0;
+		
+		for (int i =0; i <7; i++) {
+			if (array[i+1] > array[i]) {
+				temp += 1;
+			} else if (array[i+1] < array[i]) {
+				temp -= 1;
+			}			
+		}
+		
+		if (temp == 7) {
+			System.out.println("ascending");
+		} else if (temp == -7) {
+			System.out.println("descending");
+		} else {
+			System.out.println("mixed");
+		}
 	}
+	
+	//8958 OX 퀴즈
+//	public static void main(String[] args) throws Exception{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		int count = Integer.parseInt(br.readLine());
+//		String line;
+//		
+//		for (int i = 1; i <= count; i++) {
+//			int score = 0;
+//			int[] array = new int[2];
+//			line = br.readLine();
+//			char[] chars = line.toCharArray();
+//			for (char c : chars) {
+//				if(c == 'O') {
+//					array[0] += 1;
+//					score += array[0];
+//				} else if(c == 'X') {
+//					array[1] += 1;
+//					array[0] = 0;
+//				}
+//			}
+//			bw.write(Integer.toString(score)+"\n");
+//		}
+//		bw.flush();
+//		bw.close();
+//	}
 
 	//2577 숫자 세기
 //	public static void main(String[]args) throws Exception{
