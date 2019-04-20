732594e6da96c55aac0270725f8fec274cae068c
uakbuak <pchmarine@naver.com>
2019-04-20 20:58:01 +0900

    #11654 아스키 코드로 변환

---
+++ algorithm/src/algorithm/Strings.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Strings {
+
+	//11654 아스키 코드
+		public static void main(Strings[] args) {
+			Scanner scan = new Scanner(System.in);
+			char a  = scan.next().charAt(0);
+			System.out.println((int) a);
+		}
+}
