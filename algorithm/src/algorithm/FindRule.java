930a2eaf41f6caead450484da4fca95759e819b3
uakbuak <pchmarine@naver.com>
2019-04-22 19:13:48 +0900

    #1011 알파 센타우리 B

--- algorithm/src/algorithm/FindRule.java
+++ algorithm/src/algorithm/FindRule.java
@@ -8,39 +8,74 @@ import java.util.StringTokenizer;
 
 public class FindRule {
 	
-	//1011 알파 센타우리 풀이 A (시간 초과)
+	//1011 알파 센타우리 풀이 B
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int repeat = Integer.parseInt(br.readLine());
 		int x, y, distance;
-	
-		for (int i=1; i<=repeat; i++) {
-			int count = 0;
-			int come = 1;
-			int go = 1;
-			int location = 0;
-
+		int n, k, set, temp;
+		
+		for (int i=1; i <= repeat; i++) {
+			n = 2;
+			k = 4;
+			set = 1;
+			temp = 1;
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			x = Integer.parseInt(st.nextToken());
 			y = Integer.parseInt(st.nextToken());
 			distance = y-x;
 			
-			while(location < distance) {
-				location += come;
-				come += 1;
-				count += 1;
-				if(location < distance) {
-					location += go;
-					go += 1;
-					count += 1;
-				} else break; 
+			while(distance > n) {
+				n += k;
+				k += 2;
+				set += 2;
+				temp ++;
 			}
-			bw.write(Integer.toString(count)+"\n");			
+			
+			if(distance > Math.pow(temp, 2)) {
+				set ++;
+			}
+			bw.append(Integer.toString(set)+"\n");
 		}
 		bw.flush();
 		bw.close();
 	}
+	
+	//1011 알파 센타우리 풀이 A (시간 초과)
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		int repeat = Integer.parseInt(br.readLine());
+//		int x, y, distance;
+//	
+//		for (int i=1; i<=repeat; i++) {
+//			int count = 0;
+//			int come = 1;
+//			int go = 1;
+//			int location = 0;
+//
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			x = Integer.parseInt(st.nextToken());
+//			y = Integer.parseInt(st.nextToken());
+//			distance = y-x;
+//			
+//			while(location < distance) {
+//				location += come;
+//				come += 1;
+//				count += 1;
+//				if(location < distance) {
+//					location += go;
+//					go += 1;
+//					count += 1;
+//				} else break; 
+//			}
+//			bw.append(Integer.toString(count)+"\n");			
+//		}
+//		bw.flush();
+//		bw.close();
+//	}
 	
 	//1193 분수 찾기
 //	public static void main(String[] args) throws Exception {
