476c5988a5e898fca4affeb4ba22d89c8840a956
uakbuak <pchmarine@naver.com>
2019-04-22 22:05:18 +0900

    #10250 호텔 방배정

--- algorithm/src/algorithm/FindRule.java
+++ algorithm/src/algorithm/FindRule.java
@@ -4,42 +4,77 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class FindRule {
-	
-	//1011 알파 센타우리 풀이 B (int -> long으로 바꾸니 해결)
+	//10250 AMC 호텔
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int repeat = Integer.parseInt(br.readLine());
-		long x, y, distance;
-		long n, k, set;
-		
-		for (int i=1; i <= repeat; i++) {
-			n = 2;
-			k = 4;
-			set = 1;
-			
+		for (int i=1; i<=repeat; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			distance = y-x;
+			StringBuffer roomNumber = new StringBuffer();
+			int H = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int count = 0;
+			boolean off = false;
 			
-			while(distance > n) {
-				n += k;
-				k += 2;
-				set += 2;
+			for(int w=1; w<=W; w++) {
+				for(int h=1; h<=H; h++) {
+					count += 1;
+					if (count == N) {
+						if (w >= 10) {
+							roomNumber.append(Integer.toString(h)).append(Integer.toString(w));
+						} else {
+							roomNumber.append(Integer.toString(h)).append("0").append(Integer.toString(w));
+						}
+						off = true;
+						break;
+					}
+				}
+				if (off) break;
 			}
-			
-			if(distance > n - (k-2)/2) {
-				set ++;
-			}
-			bw.append(Long.toString(set)+"\n");
+			bw.append(roomNumber + "\n");
 		}
 		bw.flush();
 		bw.close();
 	}
+	
+	//1011 알파 센타우리 풀이 B (int -> long으로 바꾸니 해결)
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		int repeat = Integer.parseInt(br.readLine());
+//		long x, y, distance;
+//		long n, k, set;
+//		
+//		for (int i=1; i <= repeat; i++) {
+//			n = 2;
+//			k = 4;
+//			set = 1;
+//			
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			x = Integer.parseInt(st.nextToken());
+//			y = Integer.parseInt(st.nextToken());
+//			distance = y-x;
+//			
+//			while(distance > n) {
+//				n += k;
+//				k += 2;
+//				set += 2;
+//			}
+//			
+//			if(distance > n - (k-2)/2) {
+//				set ++;
+//			}
+//			bw.append(Long.toString(set)+"\n");
+//		}
+//		bw.flush();
+//		bw.close();
+//	}
 	
 	//1011 알파 센타우리 풀이 A (시간 초과)
 //	public static void main(String[] args) throws Exception {
